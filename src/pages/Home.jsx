249bc10ef7050251be9c20bca717e3b01e335e77
UmarNurov <umar.nurov.02@gmail.com>
2022-12-11 18:07:14 +0500

    Menu Page was created and working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../components/header/Header';
 import HeroSlider from '../components/hero-slider/HeroSlider';
 import PopularMenu from '../components/popular-menu/PopularMenu';
 import ChooseUs from '../components/choose-us/ChooseUs';
+import MenuPack from '../components/menu-pack/MenuPack';
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
     <HeroSlider/>
     <PopularMenu/>
     <ChooseUs/>
+    <MenuPack/>
   </Fragment>
   );
 }
